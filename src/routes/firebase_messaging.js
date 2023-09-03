42db8fefc4b86708a7f311ef93fac3382526700a
uumaaa <ikerantonioplumaamro@gmail.com>
2023-09-03 10:36:27 -0600

    deleted

--- src/routes/firebase_messaging.js
+++
@@ -1,5 +0,0 @@
-const admin = require("firebase-admin");
-const ServiceAccount = require("../data/ServiceAccountKey.json");
-admin.initializeApp({
-  credential: admin.credential.cert(ServiceAccount),
-});
